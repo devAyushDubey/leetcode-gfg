34b70395865664b75f1b45530be07d3f27472ba6
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2025-09-12 23:37:45 +0530

    Time: 6 ms (63.25%), Space: 19.1 MB (53.73%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,9 @@
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        return ranges::any_of(s, [](char c) {
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        });
+    }
+};

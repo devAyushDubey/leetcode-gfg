b83e67e387092f9088e7e4061ca6a6af1ae93b24
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2025-12-16 00:51:12 +0530

    Time: 0 ms (100%), Space: 54.9 MB (67.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -12,9 +12,6 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if(!p && !q) return true;
-    if(!p || !q) return false;
-    const left = isSameTree(p.left, q.left);
-    const right = isSameTree(p.right, q.right);
-    return left && right && p.val === q.val;
+    if(!p || !q) return !p && !q;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) && p.val === q.val;
 };
